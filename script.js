1524958e456a92a0850b55e92b2008ff8641dbdd
Gunther Schneider <pixeloon@gmail.com>
2016-08-27 15:20:25 -0700

    procfile added

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
 $(document).ready(function(){
 
   $(".page1").mouseenter(function(){
-    $(".page1").css("background-image", "url('images/appletes_text.png')");
+    $(".page1").css("background-image", "url('https://s3.amazonaws.com/pixeloon/portfolio/appletes_text.png')");
     $("page1-desc").css("visibility", "visible")
   }).mouseleave(function(){
-    $(".page1").css("background-image", "url('images/appletes_thumb.png')");
+    $(".page1").css("background-image", "url('https://s3.amazonaws.com/pixeloon/portfolio/appletes_thumb.png')");
   })
 
     $(".page2").mouseenter(function(){
-    $(".page2").css("background-image", "url('images/vision_text.png')");
+    $(".page2").css("background-image", "url('https://s3.amazonaws.com/pixeloon/portfolio/vision_text.png')");
     $("page2-desc").css("visibility", "visible")
   }).mouseleave(function(){
-    $(".page2").css("background-image", "url('images/vision_thumb.png')");
+    $(".page2").css("background-image", "url('https://s3.amazonaws.com/pixeloon/portfolio/vision_thumb.png')");
   })
 
     $(".page3").mouseenter(function(){
-    $(".page3").css("background-image", "url('images/rebel_text.png')");
+    $(".page3").css("background-image", "url('https://s3.amazonaws.com/pixeloon/portfolio/rebel_text.png')");
     $("page3-desc").css("visibility", "visible")
   }).mouseleave(function(){
-    $(".page3").css("background-image", "url('images/rebel_thumb.png')");
+    $(".page3").css("background-image", "url('https://s3.amazonaws.com/pixeloon/portfolio/rebel_thumb.png')");
   })
 
 
